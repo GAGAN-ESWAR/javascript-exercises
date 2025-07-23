3a48e648c6ecf9df567f8f6f5d11bec217c353c7
GAGAN-ESWAR <gaganeswar.26@gmail.com>
2025-07-23 14:49:15 +0530

    repeatString
    Write a function to add string n number of times

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if(num<0) return "ERROR";
+    let string="";
+    for(let n=0;n<num;n++){
+        string+=str;
+    }
+    return string;
 };
 
 // Do not edit below this line
