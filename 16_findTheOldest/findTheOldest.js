f8816d12615c1064fc8d27051be96039bdd8d274
GAGAN-ESWAR <gaganeswar.26@gmail.com>
2025-07-30 10:35:57 +0530

    find the oldest person
    sort the array from oldest to youngest. if the death
    year is unknown use the vurrent year

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+    if(!person.yearOfDeath){
+        person.yearOfDeath = new Date().getFullYear();
+    }
+    return person.yearOfDeath-person.yearOfBirth;
+}
 
+const findTheOldest = function(bio) {
+    bio.sort((a,b)=> getAge(b)-getAge(a));
+    return bio[0];
 };
 
 // Do not edit below this line
