8fc0aa3d7493ba7f4186e47eb95423e26633cefa
GAGAN-ESWAR <gaganeswar.26@gmail.com>
2025-07-30 09:41:47 +0530

    check if palindromes
    remove all extra characters other than numbers and
    letters. thejn reverse it and check whether same

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let alphaNumeric ="abcdefghijklmnopqrstuvwxyz0123456789";
 
+    let cleanedString = str
+    .toLowerCase()
+    .split("")
+    .filter(char => alphaNumeric.includes(char))
+    .join("");
+
+    let reversedString = cleanedString.split("").reverse().join("");
+
+    return reversedString === cleanedString;
 };
 
 // Do not edit below this line
