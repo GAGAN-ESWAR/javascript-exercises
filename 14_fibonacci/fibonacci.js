3eaba794589081e25cf3ec68549cd7d22f94f50f
GAGAN-ESWAR <gaganeswar.26@gmail.com>
2025-07-30 09:54:44 +0530

    fibonacci at index
    reccursion method

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(inp) {
+    if(inp<0) return "OOPS";
+    else if(inp==1) return 1;
+    else if(inp==0) return 0;
+    else return fibonacci(Number(inp)-1)+fibonacci(Number(inp)-2);
 };
 
 // Do not edit below this line
